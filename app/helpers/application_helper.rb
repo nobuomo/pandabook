1bc6770e3db358e273ad49c22fe354111d15f041
Your Name <you@example.com>
2017-03-16 16:26:48 +0900

    image_upload

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,17 @@
 module ApplicationHelper
-  def profile_img(user)
-    return image_tag(user.avatar, alt: user.name) if user.avatar?
+def profile_img(user)
+  return image_tag(user.avatar, alt: user.name) if user.avatar?
 
-    unless user.provider.blank?
+  unless user.provider.blank?
       img_url = user.image_url
-    else
+  else
       img_url = 'no_image.png'
-    end
+  end
     image_tag(img_url, alt: user.name)
   end
 
-  def image_img(task)
-      return image_tag(task.image, alt: task.title) if task.image?
-
+  def image_img(topic)
+      return image_tag(topic.image, alt: topic.title) if topic.image?
   end
 end
 
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,5 +2,7 @@ class Topic < ActiveRecord::Base
   validates :title, presence: true
   belongs_to :user
 
+  mount_uploader :image, AvatarUploader
+
   has_many :comments, dependent: :destroy
 end
