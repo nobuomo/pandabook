1791295dd481587611c5c73eaf91c4aa533d09fd
Your Name <you@example.com>
2017-03-26 20:38:34 +0900

    fix topic_controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,59 +2,53 @@ class TopicsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_topic, only: [:show, :edit, :update, :destroy, :confirm]
 
- def index
-   @topics=Topic.all
- end
-
- def show
-   @comment = @topic.comments.build
-   @comments = @topic.comments
-end
-
- def new
-     if params[:back]
-       @topic = Topic.new(topics_params)
-     else
-       @topic = Topic.new
-     end
+  def index
+     @topics=Topic.all
   end
 
-
- def create
-   @topic=Topic.create(topics_params)
-   @topic.user_id = current_user.id
-
-   if @topic.save
-        redirect_to topics_path, notice: "作成しました！"
-        NoticeMailer.sendmail_topic(@topic).deliver
-      else
-        render 'new'
-      end
- end
-
- def edit
- end
-
- def confirm
-   @topic = Topic.new(topics_params)
-   render :new if @topic.invalid?
- end
-
- def update
-      @topic.update(topics_params)
-      redirect_to topics_path, notice: "編集しました！"
- end
-
-def destroy
-        @topic.destroy
-        redirect_to topics_path, notice: "削除しました！"
+  def show
+     @comment = @topic.comments.build
+     @comments = @topic.comments
   end
 
+   def new
+       if params[:back]
+         @topic = Topic.new(topics_params)
+       else
+         @topic = Topic.new
+       end
+    end
 
 
+   def create
+     @topic=Topic.create(topics_params)
+     @topic.user_id = current_user.id
 
+     if @topic.save
+          redirect_to topics_path, notice: "作成しました！"
+          NoticeMailer.sendmail_topic(@topic).deliver
+        else
+          render 'new'
+        end
+   end
 
-end
+   def edit
+   end
+
+   def confirm
+     @topic = Topic.new(topics_params)
+     render :new if @topic.invalid?
+   end
+
+   def update
+        @topic.update(topics_params)
+        redirect_to topics_path, notice: "編集しました！"
+   end
+
+   def destroy
+          @topic.destroy
+          redirect_to topics_path, notice: "削除しました！"
+   end
 
  private
     def topics_params
@@ -64,3 +58,5 @@ end
     def set_topic
           @topic = Topic.find(params[:id])
     end
+
+end
