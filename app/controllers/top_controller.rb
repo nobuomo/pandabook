594b935da99b574eb9217aa0f3aa00483db74e17
Your Name <you@example.com>
2017-04-07 15:07:58 +0900

    fixing top.messsage issue

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,14 +1,31 @@
 class TopController < ApplicationController
-  def index
-    @users = User.all
-  end
 
   before_action :authenticate_user!
   before_action :set_topic, only: [:show, :edit, :update, :destroy]
+  before_action do
+    @conversation = Conversation.find(params[:conversation_id])
+  end
 
   def index
      @topics=Topic.all
+     @users = User.all
+     @messages = @conversation.messages
+        if @messages.length > 10
+          @over_ten = true
+          @messages = @messages[-10..-1]
+        end
+
+        if params[:m]
+          @over_ten = false
+          @messages = @conversation.messages
+        end
+
+        if @messages.last
+          if @messages.last.user_id != current_user.id
+            @messages.last.read = true
+        end
   end
+
 
   def show
      @comment = @topic.comments.build
@@ -34,6 +51,27 @@ class TopController < ApplicationController
         else
           render 'new'
         end
+        # ログインユーザーに紐付けてインスタンス生成するためbuildメソッドを使用します。
+        @comment = current_user.comments.build(comment_params)
+        @topic = @comment.topic
+
+        # クライアント要求に応じてフォーマットを変更
+      respond_to do |format|
+          if @comment.save
+            format.html { redirect_to topic_path(@topic), notice: 'コメントを投稿しました。' }
+            format.json { render :show, status: :created, location: @comment }
+           # JS形式でレスポンスを返します。
+            format.js { render :index }
+          else
+            format.html { render :new }
+            format.json { render json: @comment.errors, status: :unprocessable_entity }
+          end
+       end
+
+      @message = @conversation.messages.build(message_params)
+      if @message.save
+      redirect_to conversation_messages_path(@conversation)
+      end
    end
 
    def edit
@@ -54,39 +92,6 @@ class TopController < ApplicationController
           redirect_to topics_path, notice: "削除しました！"
    end
 
-   before_action do
-     @conversation = Conversation.find(params[:conversation_id])
-   end
-
-   def index
-       @messages = @conversation.messages
-         if @messages.length > 10
-           @over_ten = true
-           @messages = @messages[-10..-1]
-         end
-
-         if params[:m]
-           @over_ten = false
-           @messages = @conversation.messages
-         end
-
-         if @messages.last
-           if @messages.last.user_id != current_user.id
-             @messages.last.read = true
-           end
-         end
-
-     @message = @conversation.messages.build
-   end
-
-   def create
-     @message = @conversation.messages.build(message_params)
-     if @message.save
-       redirect_to conversation_messages_path(@conversation)
-     end
-   end
-
-
 
  private
     def topics_params
@@ -97,8 +102,12 @@ class TopController < ApplicationController
           @topic = Topic.find(params[:id])
     end
 
+    def comment_params
+      params.require(:comment).permit(:topic_id, :content)
+    end
+
     def message_params
       params.require(:message).permit(:body, :user_id)
     end
-
+ end
 end
