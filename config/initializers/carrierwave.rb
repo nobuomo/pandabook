7cae8c2936a03132fcc0e5ad40ab8b8170572fbb
Your Name <you@example.com>
2017-05-17 21:55:02 +0900

    Make Carrierwave work on Heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,24 +1,42 @@
+# CarrierWave.configure do |config|
+#   config.fog_credentials = {
+#       provider:              'AWS',
+#       aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+#       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+#       region:                'ap-northeast-1',
+#       path_style:            true,
+#   }
+#
+#   config.storage = :fog
+#   config.fog_authenticated_url_expiration = 60
+#   config.fog_public     = true
+#   config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
+#   config.cache_storage = :fog
+#
+#   case Rails.env
+#     when 'production'
+#       config.fog_directory = 'panbook_production'
+#       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/panbook_production'
+#     when 'development'
+#       config.fog_directory = 'panbook_development'
+#       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/panbook_development'
+#   end
+# end
+
 CarrierWave.configure do |config|
+  config.root = Rails.root.join('tmp') # adding these...
+  config.cache_dir = 'carrierwave' # ...two lines
+  # config.cache_dir = "#{Rails.root}/tmp/uploads"
+
   config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region:                'ap-northeast-1',
-      path_style:            true,
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => 'key',                        # required
+    :aws_secret_access_key  => 'secret',                     # required
+    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+    :host                   => 's3.example.com',             # optional, defaults to nil
+    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
-
-  config.storage = :fog
-  config.fog_authenticated_url_expiration = 60
-  config.fog_public     = true
-  config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
-  config.cache_storage = :fog
-
-  case Rails.env
-    when 'production'
-      config.fog_directory = 'panbook_production'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/panbook_production'
-    when 'development'
-      config.fog_directory = 'panbook_development'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/panbook_development'
-  end
+  config.fog_directory  = 'directory'                             # required
+  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
