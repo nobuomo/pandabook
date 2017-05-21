18805bccc9f2c9ca1e44b6e4b5724a9cc76b56b8
Your Name <you@example.com>
2017-05-21 10:00:47 +0900

    fix fog_credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -28,14 +28,14 @@ CarrierWave.configure do |config|
   config.cache_dir = 'carrierwave' # ...two lines
   # config.cache_dir = "#{Rails.root}/tmp/uploads"
 
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],                        # required
-    :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],                     # required
-    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-    :host                   => 's3.example.com',             # optional, defaults to nil
-    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
-  }
+    config.fog_credentials = {
+        provider:              'AWS',
+        aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+        region:                'ap-northeast-1',
+        path_style:            true,
+    }
+
   config.fog_directory  = 'directory'                             # required
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
