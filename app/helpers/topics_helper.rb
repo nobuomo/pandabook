627ecbe809ddd761f5c2e9188f986b2cabab2e0d
Your Name <you@example.com>
2017-06-05 08:36:17 -0500

    fix confirm for upload

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,7 +1,8 @@
 module TopicsHelper
     def choose_new_or_edit
-      if action_name == 'new' || action_name == 'confirm'
-        confirm_topics_path
+      if action_name == 'new' 
+
+        topic_path
       elsif action_name == 'edit'
         topic_path
       end
