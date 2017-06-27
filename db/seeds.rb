fbf33a4db420ebc0d93f14611e839c68243f6228
Your Name <you@example.com>
2017-06-27 22:33:21 +0900

    fix users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,27 @@
-# 5.times do |n|
-#   email = Faker::Internet.email
-#   password = "password"
-#   User.create!(email: email,
-#                password: password,
-#                password_confirmation: password,
-#                )
+5.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
+#
+# User.seed(:id) do |s|
+#   s.id = 1
+#   s.name = "jon"
+#   s.email = "ppp@com"
+#   s.password = "password"
+#   s.uid = "pamda"
+#   s.provider ="yama"
+#
 # end
-
-User.seed(:id) do |s|
-  s.id = 1
-  s.name = "jon"
-  s.email = "ppp@com"
-  s.password = "password"
-  s.uid = "pamda"
-  s.provider ="yama"
-
-end
-
-User.seed(:id) do |s|
-  s.id = 2
-  s.name = "emily"
-  s.email = "qqq@com"
-  s.password = "password"
-  s.uid ="kame"
-  s.provider ="kawa"
-end
+#
+# User.seed(:id) do |s|
+#   s.id = 2
+#   s.name = "emily"
+#   s.email = "qqq@com"
+#   s.password = "password"
+#   s.uid ="kame"
+#   s.provider ="kawa"
+# end
