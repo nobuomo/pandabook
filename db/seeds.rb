6de7227dc7402fc1b4d93c479a186ecc0fddf773
Your Name <you@example.com>
2017-06-27 22:40:50 +0900

    fix users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,3 @@
                password_confirmation: password,
                )
 end
-#
-# User.seed(:id) do |s|
-#   s.id = 1
-#   s.name = "jon"
-#   s.email = "ppp@com"
-#   s.password = "password"
-#   s.uid = "pamda"
-#   s.provider ="yama"
-#
-# end
-#
-# User.seed(:id) do |s|
-#   s.id = 2
-#   s.name = "emily"
-#   s.email = "qqq@com"
-#   s.password = "password"
-#   s.uid ="kame"
-#   s.provider ="kawa"
-# end
