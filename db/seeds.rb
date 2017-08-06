85f99ede9e73b1efc4cad4b7cf89b18f1a71112b
Your Name <you@example.com>
2017-08-06 14:45:30 +0900

    fix follow

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,28 @@
 
 
 
-# 10.times do |n|
-#   email = Faker::Internet.email
-#   password = "password"
-#   name = Faker::Name.name
-#
-#   uid ="uid123r#{n}"
-#   provider = "pandarbook#{n}"
-#   User.create!(email: email,
-#                password: password,
-#                password_confirmation: password,
-#                name: name,
-#
-#                uid: uid,
-#                provider: provider,
-#                )
-#
-#     10.times do |no|
-#       Topic.create(:title => "title #{no}", :user_id => 1, :content => "pan #{no}")
-#
-#     end
+10.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  name = Faker::Name.name
+
+  uid ="uid123r#{n}"
+  provider = "pandarbook#{n}"
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               name: name,
+
+               uid: uid,
+               provider: provider,
+               )
 
     10.times do |no|
-      Comment.create(:content => "panda #{no}", :user_id =>1, :topic_id => 11)
+      Topic.create(:title => "title #{no}", :user_id => 17, :content => "pan #{no}")
+
+    end
+
+    10.times do |no|
+      Comment.create(:content => "panda #{no}", :user_id =>17, :topic_id => 94)
     end
 # end
